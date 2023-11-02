de3e0791f0f848d4df36301daf8d7d02b2368df4
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-11-02 08:05:42 +0530

    add custom header files

---
+++ custom.h
@@ -0,0 +1,4 @@
+int sum(int*f_num,  int* s_num){
+    int sum = * f_num + *s_num;
+    return sum;
+}
---
+++ f_program.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include"custom.h"
+int main(){
+    printf("Hello \n");
+    int num1 = 8;
+    int num2 = 10;
+    int result = sum(&num1, &num2);
+    printf("%d\n", result);
+}
